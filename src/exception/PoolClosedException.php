e58f653143dbd6ab9df1847edab03b12113e5ab7
Ye Wenbin <yewenbin@winwin-inc.com>
2022-03-22 18:43:59 +0800

    add release call in PoolInterface

---
+++ src/exception/PoolClosedException.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the Kuiper package.
+ *
+ * (c) Ye Wenbin <wenbinye@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace kuiper\swoole\exception;
+
+class PoolClosedException extends \Exception
+{
+    /**
+     * PoolTimeoutException constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct('Cannot get connection because pool is closed');
+    }
+}
